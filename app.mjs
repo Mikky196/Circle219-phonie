9d283ccda239e5e199f6b079c5bf9bc2467a4bcc
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2022-07-26 18:43:10 +0000

    restore miraculously deleted code

--- app.mjs
+++ app.mjs
@@ -1,17 +1,22 @@
 function startApp() {
-    // Your entire app should not necessarily be coded inside this 
-    // single function (though there's no penalty for that), 
-    // so create and use/call additional functions from here
   
-    // pls remove the below and make some magic in here!
-    console.log('make magic in here!');
+  const header = document.querySelector('h1');
   
-    const header = document.querySelector('h2');
-    if(header) {
-      header.textContent = 'make some magic here!!';
-    }
+  if (header) {
+    header.textContent = 'Phonie!!';
+  }
+};
+
+export function getNetworkOperators() {
+  return {
+    MTN: ["0702", "0703", "0706", "0803", "0806", "0810", "0813", "0814", "0816", "0903", "0906"],
+    AIRTEL: ["0701", "0708", "0802", "0808", "0812", "0902", "0907", "0901"],
+    '9mobile': ["0809", "0817", "0818", "0908", "0909"],
+    GLO: ["0705", "0805", "0807", "0811", "0815", "0905"],
+    NTEL: ["0804", "234804", "+234804"],
   };
-  
-  // ======= DO NOT EDIT ============== //
-  export default startApp;
-  // ======= EEND DO NOT EDIT ========= //
+}
+
+// ======= DO NOT EDIT ============== //
+export default startApp;
+// ======= EEND DO NOT EDIT ========= //
